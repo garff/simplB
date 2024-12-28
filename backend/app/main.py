ae45f7ce7770cb7f11011d6ab1a2aa19fa2a96d1
madsgarff@hotmail.com <madsh19@sdu.student.com>
2024-12-28 14:03:41 +0100

    add: standard / endpoint serving blog posts.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from utils.utils import get_blog_post_file_names, get_blog_post_content
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def blogposts():
+    blog_post_file_names = get_blog_post_file_names()
+    return { file_name:get_blog_post_content(file_name) for file_name in blog_post_file_names }
