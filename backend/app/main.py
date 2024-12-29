4a172a6853d3b3cbc657cf36eff31c2374d373f8
madsgarff@hotmail.com <madsh19@sdu.student.com>
2024-12-29 10:06:52 +0100

    done: backend.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,12 @@
+"""Main module for the backend."""
 from fastapi import FastAPI
-from utils.utils import get_blog_post_file_names, get_blog_post_content
+
+from .utils.utils import get_blog_post_content, get_blog_post_file_names
 
 app = FastAPI()
 
 @app.get("/")
 def blogposts():
+    """Blog posts get route."""
     blog_post_file_names = get_blog_post_file_names()
     return { file_name:get_blog_post_content(file_name) for file_name in blog_post_file_names }
--- backend/app/utils/utils.py
+++ backend/app/utils/utils.py
@@ -1,4 +1,5 @@
-import os 
+"""Utils module for the backend."""
+import os
 
 CURRENT_FP = os.path.abspath(__file__)
 CURRENT_DIR = os.path.dirname(CURRENT_FP)
@@ -6,9 +7,8 @@ PARRENT_DIR = os.path.dirname(CURRENT_DIR)
 BLOG_POSTS_DIR = os.path.join(PARRENT_DIR, "blog_posts")  
 
 def get_blog_post_file_names() -> list[str]:
+    """Returns a list of blog post file names."""
     blogpost_file_names = []
-
-    print(BLOG_POSTS_DIR)
 
     for _, _, files in os.walk(BLOG_POSTS_DIR):
         for file in files:
@@ -17,5 +17,6 @@ def get_blog_post_file_names() -> list[str]:
     return blogpost_file_names
 
 def get_blog_post_content(file_name: str) -> str:
-    with open(os.path.join(BLOG_POSTS_DIR, file_name), "r") as file:
+    """Returns the content of a blog post."""
+    with open(os.path.join(BLOG_POSTS_DIR, file_name), 'r', encoding='utf-8') as file:
         return file.read()
