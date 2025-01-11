a8986a0298476b01e3a907e1c1b2ada5478524f7
Mads Garff <mads@garff.dk>
2025-01-11 15:20:56 +0100

    add: sidebar, refactor footer element.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,26 @@
 import Posts from '../components/posts/posts';
 import { Suspense } from 'react';
 import Loading from './loading';
+import Sidebar from '@/components/sidebar/sidebar';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Suspense fallback={<Loading />}>
-          <Posts />
-        </Suspense>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
+    <div className="justify-items-center min-h-screen pb-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="grid grid-cols-1 gap-4">
+        <main className="pt-32 flex justify-center sm:items-start">
+          <Suspense fallback={<Loading />}>
+            <Posts />
+          </Suspense>
+        </main>
+      </div>
+      <Sidebar />
+      <footer className="w-full fixed bottom-0 pb-4 flex justify-center items-center">
+        <a
+          href="https://github.com/garff"
+          target="_blank">
+          Mads Garff&trade; @ 2025
+        </a>
+      </footer>
     </div>
   );
 }
